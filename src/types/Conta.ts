4876ad5f51802670d77a1520f46fde4f71100107
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-12-04 17:00:34 -0300

    Modificadores de acesso

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,11 +1,11 @@
-import { GrupoTransacao } from "./GrupoTransacao";
-import { TipoTransacao } from "./TipoTransacao";
-import { Transacao } from "./Transacao";
+import { GrupoTransacao } from "./GrupoTransacao.js";
+import { TipoTransacao } from "./TipoTransacao.js";
+import { Transacao } from "./Transacao.js";
 
 export class Conta {
-  nome: string;
-  saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
-  transacoes: Transacao[] =
+  protected nome: string;
+  protected saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+  private transacoes: Transacao[] =
     JSON.parse(
       localStorage.getItem("transacoes"),
       (key: string, value: any) => {
@@ -17,6 +17,10 @@ export class Conta {
     ) || [];
   constructor(nome: string) {
     this.nome = nome;
+  }
+
+  public getTitular() {
+    return this.nome;
   }
 
   getGruposTransacoes(): GrupoTransacao[] {
