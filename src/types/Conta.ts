8b8ff22f0ae8a331c958c8919fd8e034739a0704
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-12-04 16:14:41 -0300

    Classes

---
+++ src/types/Conta.ts
@@ -0,0 +1,23 @@
+import { Transacao } from "./Transacao";
+
+export class Conta {
+  nome: string;
+  saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+  transacoes: Transacao[] =
+    JSON.parse(
+      localStorage.getItem("transacoes"),
+      (key: string, value: any) => {
+        if (key === "data") {
+          return new Date(value);
+        }
+        return value;
+      }
+    ) || [];
+  constructor(nome: string) {
+    this.nome = nome;
+  }
+}
+
+const conta = new Conta("Joana da Silva Oliveira");
+
+export default conta;
